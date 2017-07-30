4224bcd1e6ac8f06d88eba01c5f1a75382ff1fce
Surya Dantuluri <surya.mvhs@gmail.com>
2017-07-30 12:31:51 -0700

    update

---
+++ Dice.java
@@ -0,0 +1,14 @@
+//  Your Name Here
+//  8/20/2015
+//  Dice.java
+//  Simulates a single die.  The die
+//  has a number of sides, and it
+//  will keep a roll count.  The die
+//  also has a face value.  This
+//  face value and the rollcount are
+//  accessible by calling methods.
+
+public class Dice
+{
+
+}
---
+++ Jackpot.java
@@ -0,0 +1,17 @@
+//  Surya Dantuluri
+//  8/20/2017
+//  Jackpot.java
+//  Prompt the user to enter the number of
+//  sides for the 3 dice to be rolled.  This
+//  user input will be checked, and the user
+//  will be reprompted if they make an error.
+//  The 3 dice are then constructed using
+//  the value entered by the user.  The dice
+//  are then rolled until all 3 dice show the
+//  same face value.  A count is also recorded
+//  and shown as the 3 dice are rolled.
+
+public class Jackpot
+{
+
+}
