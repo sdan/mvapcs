15911ffde35f12cf519214750388fa26d5a688af
Surya Dantuluri <surya.mvhs@gmail.com>
2017-08-20 18:39:05 -0700

    update

--- MySummer/MySummer.java
+++ MySummer/MySummer.java
@@ -1,23 +1,25 @@
-/* Surya Dantuluri
+/** Surya Dantuluri
  * 8/14/2017
- * 
- * 
+ *
+ *
  */
-
 public class MySummer
 {
-	public static void main (String args[])
-		{
-		System.out.println("\n\n\nThis summer I relaxed and had fun learning some coding");
-		System.out.println("and learning some chemistry for this schoool year. Although,");
-		System.out.println("I didn't go on vacation to any other country or state, my mom");
-		System.out.println("and I took a small trip down to Los Angeles to take a look at");
-		System.out.println("some colleges such as UCLA and CalTech. During the summer");
-		System.out.println("I also spent time learning C++ for the first time to aid me");
-		System.out.println("learning algorithms for problem solving. During the last few");
-		System.out.println("weeks of summer I spent time running and getting into shape for");
-		System.out.println(" the upcoming Cross Country season as well as learning some problem");
-		System.out.println("solving skills for upcoming math contests/school year.\n\n\n");	
-		}
+    public static void main (String args[])
+        {
+        System.out.println("\n\n\nThis summer I relaxed and had fun learning some coding");
+        System.out.println("and learning some chemistry for this schoool year. Although,");
+        System.out.println("I didn't go on vacation to any other country or state, my mom");
+        System.out.println("and I took a small trip down to Los Angeles to take a look at");
+        System.out.println("some colleges such as UCLA and CalTech. During the summer");
+        System.out.println("I got the chance to present alongside Google's Research Director");
+        System.out.println("and Stefano Ermon, a Stanford Professor on Generative");
+        System.out.println("Adversarial Networks for the High School AI Labratory's Online Convention.");
+        System.out.println("I also spent time learning C++ for the first time to aid me");
+        System.out.println("learning algorithms for problem solving. During the last few");
+        System.out.println("weeks of summer I spent time running as well as building an");
+        System.out.println("autonomous robot for an upcoming competition in Canada. I also ");
+        System.out.println("spent time doing past competition problems for the upcoming");
+        System.out.println("math contests/school year.  \n\n\n");
+        }
 }
-
