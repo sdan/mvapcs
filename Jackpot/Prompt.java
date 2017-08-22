8603205d403f8800e273aa4e0e088e30ec0a2394
Surya Dantuluri <surya.mvhs@gmail.com>
2017-08-21 17:16:48 -0700

    update

--- Jackpot/Prompt.java
+++ Jackpot/Prompt.java
@@ -1,12 +1,13 @@
-//  Surya Dantuluri
-//  8/20/2017
-//  Prompt.java
-//  Provide some utilities for user input.  This
-//  class enhances the Scanner class, so that our
-//  client classes can recover from "bad" input,
-//  such as input outside of described parameters
-//  and input in the wrong format (such as a
-//  String instead of an int).
+// //* Surya Dantuluri
+// *  8/20/2017
+// *  Prompt.java
+// *  Provide some utilities for user input.  This
+// * class enhances the Scanner class, so that our
+// * client classes can recover from "bad" input,
+// * such as input outside of described parameters
+// * and input in the wrong format (such as a
+// *  String instead of an int).
+// */
 import java.util.Scanner;
 
 public class Prompt
@@ -27,36 +28,38 @@ public class Prompt
     int val = 0;
     do
     {
-      bool valid=false;
+      valid=false;
       input = getString(ask);
       try
       {
-         val= Integer.parseInt(input);
+        val= Integer.parseInt(input);
       }
       catch(NumberFormatException e)
       {
         valid = true;
-      }    }
-      while(valid);
+      }
     }
-
-    /*  Prompts the user and picks up an int.  Checks for
-	 *  "bad" input and reprompts if not an int.  Also checks
-	 *  for input within a given range, and reprompts if outside
-	 *  that range.
-	 *  @param ask       The String prompt to be displayed to the user.
-	 *  @param min       The minimum integer value to be allowed as input.
-	 *  @param max       The maximum integer value to be allowed as input.
-	 *  @return          The int entered by the user.
-	 */
-	public static int getInt(String ask, int min, int max)
-	{
-		int val = 0;
-		do
-		{
-			val = getInt(ask + " (from " + min + " to " + max + "): ");
-		}
-		while(val < min || val > max);
-		return val;
-	}
+    while(valid);
+    return val;
   }
+
+  /*  Prompts the user and picks up an int.  Checks for
+  *  "bad" input and reprompts if not an int.  Also checks
+  *  for input within a given range, and reprompts if outside
+  *  that range.
+  *  @param ask       The String prompt to be displayed to the user.
+  *  @param min       The minimum integer value to be allowed as input.
+  *  @param max       The maximum integer value to be allowed as input.
+  *  @return          The int entered by the user.
+  */
+  public static int getInt(String ask, int min, int max)
+  {
+    int val = 0;
+    do
+    {
+      val = getInt(ask + " (from " + min + " to " + max + "): ");
+    }
+    while(val < min || val > max);
+    return val;
+  }
+}
