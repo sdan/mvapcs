65f7f23b3d2534015d4a4d51b048afff9c923550
Surya Dantuluri <surya.mvhs@gmail.com>
2017-08-22 14:03:05 -0700

    update

--- OpenFile/OpenFile.java
+++ OpenFile/OpenFile.java
@@ -7,61 +7,54 @@
  * @since August 22, 2017
  *
  */
-
  import java.util.Scanner;
  import java.io.File;
  import java.io.FileNotFoundException;
  import java.io.PrintWriter;
-
  public class OpenFile{
-
-	 public static void main(String[]args){
-		Scanner infile = OpenFile.openToRead("g.txt");
-		PrintWriter outfile = OpenFile.openToWrite("gcopy.txt");
-		String temp = null;
-		while(infile.hasNext()){
-			temp = infile.nextLine();
-			System.out.println(temp);
-			outfile.println(temp);
-		}
-		infile.close();
-		outfile.close();
-	 }
-
-	 /**
-	  * Oepn a file for reading.
-	  * @param fileString     The name of the file to be opened
-	  * @return 	          A Scanner instance of a file to be open.
-	  */
-	 public static Scanner openToRead(String fileString){
-		Scanner fromfile = null;
-		File filename = new File(fileString);
-
-		try{
-			fromfile = new Scanner(filename);
-		}catch(FileNotFoundException e){
-			System.out.println("\n\nSorry the file could not be found.\n\n");
-			System.exit(-1);
-		}
-		return fromfile;
-	 }
-
-	 /**
-	  * Oepn a file for writing.
-	  * @param fileString     The name of the file to be opened to be created.
-	  * @return 	          A PrintWriter instance of a file to be opened(created).
-	  */
-	 public static PrintWriter openToWrite(String fileString){
-	 	PrintWriter outfile = null;
-		try{
-			outfile = new PrintWriter(fileString);
-		}catch(Exception e){
-			System.out.println("\n\nSorry the file could not be created.\n\n");
-			System.exit(-1);
-		}
-		return outfile;
-
-	 }
-
+     public static void main(String[]args){
+        Scanner fromfile = OpenFile.openToRead("g.gxt");
+        PrintWriter outfile = OpenFile.openToWrite("gcopy.txt");
+        String temp = null;
+        while(fromfile.hasNext())
+        {
+            temp = fromfile.nextLine();
+            System.out.println(temp);
+            outfile.println(temp);
+        }
+        fromfile.close();
+        outfile.close();
+     }
+     /**
+      * Open a file for reading.
+      * @param filestring     The name of the file to be opened
+      * @return               A Scanner instance of a file to be open.
+      */
+     public static Scanner openToRead(String filestring){
+        Scanner fromfile = null;
+        File filename = new File(filestring);
+        try{
+            fromfile = new Scanner(filename);
+        }catch(FileNotFoundException e){
+            System.out.println("\n\nSorry the file could not be found.\n\n");
+            System.exit(1);
+        }
+        return fromfile;
+     }
+     /**
+      * Open a file for writing.
+      * @param filestring     The name of the file to be opened to be created.
+      * @return               A PrintWriter instance of a file to be opened(created).
+      */
+     public static PrintWriter openToWrite(String filestring){
+         PrintWriter outfile = null;
+        try{
+            outfile = new PrintWriter(filestring);
+        }catch(Exception e){
+            System.out.println("\n\nSorry the file could not be created.\n\n");
+            System.exit(2);
+        }
+        return outfile;
+     }
 
  }
