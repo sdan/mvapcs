f83594fc323c9287517977753fcbb562a448bd2c
Surya Dantuluri <surya.mvhs@gmail.com>
2017-08-23 08:49:07 -0700

    update

--- find.sh
+++ find.sh
@@ -1,5 +1,12 @@
-files=($(find -E . -type f -regex "^.java$"))
-for item in ${files[java]}
+#!/bin/bash
+files=($(find -E . -type f -regex "^.*$"))
+for item in ${files[*]}
 do
   printf "   %s\n" $item
+  if [ -e Stones.java ]
+  then
+    echo "Hey that\'s a large number."
+  else
+    echo "Nope."
+  fi
 done
