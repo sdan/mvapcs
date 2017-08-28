20b10a3836456d885dfa894c35466fe085fe7fbd
Surya Dantuluri <surya.mvhs@gmail.com>
2017-08-27 18:09:18 -0700

    update

--- Stones/Stones.java
+++ Stones/Stones.java
@@ -144,6 +144,18 @@ public class Stones
     done = true;
 
 //nim sum should equal 0.
+// A B C nim-sum 
+//
+// 3 4 5 0102=210   I take 2 from A, leaving a sum of 000, so I will win.
+// 1 4 5 0002=010   You take 2 from C
+// 1 4 3 1102=610   I take 2 from B
+// 1 2 3 0002=010   You take 1 from C
+// 1 2 2 0012=110   I take 1 from A
+// 0 2 2 0002=010   You take 1 from C
+// 0 2 1 0112=310   The normal play strategy would be to take 1 from B, leaving an even number (2)
+//                  heaps of size 1.  For misère play, I take the entire B heap, to leave an odd
+//                  number (1) of heaps of size 1.
+// 0 0 1 0012=110   You take 1 from C, and lose.
 
     int calculateNimSum(int piles[], int n)
     {
