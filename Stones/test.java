0b90cc119366d5af6e172fa18982961ed58c4bbc
Surya Dantuluri <surya.mvhs@gmail.com>
2017-08-28 22:27:49 -0700

    update

--- Stones/test.java
+++ Stones/test.java
@@ -1,13 +1,19 @@
+import java.util.Scanner;
+
 public class test
 {
   public static void main(String []args)
   {
-    int pile1 = 1;
-    int pile2 = 4;
-    int pile3 = 5;
+    Scanner keyboard = new Scanner(System.in);
+		 int pile1 = keyboard.nextInt();
+     int pile2 = keyboard.nextInt();
+     int pile3 = keyboard.nextInt();
+
+
     int hello = pile1^pile2;
+    System.out.println("hello"+hello);
     int bye = pile3^hello;
-    System.out.println(bye);
+    System.out.println("bye"+bye);
 
   }
 }
