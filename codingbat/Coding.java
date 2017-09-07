c3cbdc691bb3d7f613e9f2ae3c20cf6a48cb46a0
Surya Dantuluri <surya.mvhs@gmail.com>
2017-09-07 16:45:25 -0700

    update

---
+++ codingbat/Coding.java
@@ -0,0 +1,13 @@
+public class Coding
+{
+  int data;
+  public Coding()
+  {
+    data = 1;
+  }
+  public static void main(String [] args)
+  {
+    Coding monkey = new Coding();
+    System.out.println(monkey.data);
+  }
+}
