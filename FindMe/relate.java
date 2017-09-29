ec116314fecbfad97d2bc0f20cb39bbb8f3896c9
Surya Dantuluri <surya.mvhs@gmail.com>
2017-09-28 22:45:56 -0700

    update

---
+++ FindMe/relate.java
@@ -0,0 +1,12 @@
+public class relate
+{
+  public static void main(String[]args)
+  {
+    System.out.println("7/3.5: "+7/3.5);
+    System.out.println("7.0/3.5: "+7.0/3.5);
+    double x = 7.0;
+double y = 3.5;
+if (x / y == 2.0)
+System.out.println("hello friends");
+  }
+}
