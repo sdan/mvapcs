632a1f4368eff3fbc4542622e147b4fd74e2b849
Surya Dantuluri <surya.mvhs@gmail.com>
2017-11-02 16:59:07 -0700

    aa

---
+++ SorryTest.java
@@ -0,0 +1,37 @@
+//SorryTest
+//Surya Dantuluri Test November 3 2017
+import java.util.Scanner;
+public class SorryTest
+{
+int arr[] = new int[4];
+int store[] = new int[21];
+	public static void main(String[] args) 
+	{
+		SorryTest testy = new SorryTest();
+		testy.playGame();
+	}
+	public SorryTest()
+	{
+		arr[] = {0,0,0,0};
+		store[] = {0,0,0,0};
+	}
+	public void playGame()
+	{
+	int counter = 0;
+		while(store[20])
+		{
+		movePiece();
+		checkPieces();
+		
+		}	
+		
+	}
+	public void movePieces()
+	{
+		
+	}
+	public void checkPieces()
+	{
+		
+	}
+}
