f3ff6de229cc10c551a7d158983a33c7780e064d
Surya Dantuluri <surya.mvhs@gmail.com>
2017-11-23 11:49:45 -0800

    aaa

--- Sorts/Sorts.java
+++ Sorts/Sorts.java
@@ -4,22 +4,19 @@ public class Sorts
 {
 	public static int bubbleSort (ArrayList <Integer> list)
 	{
-		System.out.println("BELLLASODIAJSDJ");
 		int steps = 0;
 		boolean swap = false;
-		int numberOfUnsortedElements = 0;
+		int numberOfUnsortedElements = list.size();
 		steps+=3; //Declaration and assignment of steps, swap, and numberOfUnsortedElements
 		do {
-			steps+=2; //Declaration of iterate and assignment of swap and iterate
+			steps+=2; //Declaration of iterate and assignment of swap
 			swap = false;
-			System.out.println("HELL:L:OISJDOAISJDOAISDJOASIJD");
 			for (int iterate = 1;iterate<numberOfUnsortedElements;iterate++)
 			{
-				System.out.println("HELL:L:OISJDOAISJDOAISDJOASIJD");
-				steps+=3; //boolean above and below, iterate++
+				steps+=2; //boolean above (when true), iterate++
 				if(list.get(iterate-1)>list.get(iterate))
 				{
-					System.out.println("HELL:L:OISJDOAISJDOAISDJOASIJD");
+					steps++; //boolean above
 					steps+=4; // Assignment of temp, swap, and swapping indexes of the ArrayList
 					int temp = list.get(iterate);
 					list.set(iterate,list.get(iterate-1));
@@ -27,7 +24,7 @@ public class Sorts
 					swap = true;
 				}
 			}
-			numberOfUnsortedElements++;
+			numberOfUnsortedElements--;
 			steps+=2; // boolean below and assignment above
 		} while (swap);
 		return steps;
@@ -64,18 +61,23 @@ public class Sorts
 
 	public static int insertionSort (ArrayList <Integer> list)
 	{
+		System.out.println("insertion");
 		int steps = 0;
-		for(int i = 1; i<list.size();i++)
+		steps++; //Declaration and assignment of outer
+		for (int outer = 1; outer < list.size(); outer++)
 		{
-			int temp = list.get(i);
-			int j = i;
-			while(j>0 && temp < list.get(j-1)){
-				list.set(j, list.get(j-1));
-				j--;
-				steps++;
+			int position = outer;
+			int key = list.get(position);
+			steps+=4; //Declaration and assignment of position, key, outer++, boolean above
+			while (position > 0 && list.get(position-1) > key)
+			{
+				steps+=2; //both boolean statements
+				list.set(position,list.get(position - 1));
+				position--;
+				steps+=2; //Assignment of position, assignment of position in set
 			}
-			list.set(j, temp);
-			steps++;
+			list.set(position,key);
+			steps++; //Assignment of position with set
 		}
 		return steps;
 	}
@@ -83,21 +85,22 @@ public class Sorts
 	public static int mergeSort (ArrayList<Integer> a, int from, int to)
 	{
 		int steps = 0;
-		if(to - from < 2){
-			steps++;
+		if(to - from < 2)
+		{
+			steps++; //boolean above
 			if(to>from && (a.get(to) < a.get(from)))
 			{
-				int aTemp = a.get(to);
-				int bTemp = a.get(from);
-				a.set(to, bTemp);
-				a.set(from, aTemp);
-				steps++;
+				steps+=2; //both boolean expresions above
+				int temp = a.get(to);
+				a.set(to, a.get(from));
+				a.set(from, temp);
+				steps+=3; //Declaration and assignment of temp, and swtiching
 			}
 		}
 		else
 		{
 			int middle = (from + to) / 2;
-
+			steps++;//Declaration and assignment of middle
 			steps += mergeSort(a, from, middle);
 			steps += mergeSort(a, middle + 1, to);
 			steps += merge(a, from, middle, to);
@@ -110,40 +113,44 @@ public class Sorts
 		ArrayList<Integer> temp = new ArrayList<Integer>(a.size());
 		int i = from, j = middle+1, k = from;
 		int steps = 0;
-		while( i <= middle && j <= to){
-			steps++;
-			if(a.get(i) < a.get(j)){
+		steps+=4 ;//Declaration and assignment of i,j,k, ArrayList temp
+		while( i <= middle && j <= to)
+		{
+			steps+=2; //both boolean expresions above
+			if(a.get(i) < a.get(j))
+			{
 				temp.set(k,a.get(i));
 				i++;
-				steps++;
+				steps+=3;//boolean expresion above, set, assignment of i
 			}
 			else
 			{
 				temp.set(k, a.get(j));
 				j++;
+				steps+=2;//set, assignment of step
 			}
 			k++;
+			steps++; //Assignment of k
 		}
-
-		while (i <= middle){
+		while (i <= middle)
+		{
 			temp.set(k, a.get(i));
 			i++;
 			k++;
-			steps++;
+			steps+=4;//boolean above, assignment of i,k,temp
 		}
-		steps++;
 
 		while(j<=to){
 			temp.set(k, a.get(j));
 			j++;
 			k++;
-			steps++;
+			steps+=4;//boolean above, assignment of j, k, temp
 		}
-		steps++;
 
-		for(k = from; k<=to;k++){
+		for(k = from; k<=to;k++)
+		{
 			a.set(k, temp.get(k));
-			steps++;
+			steps+=3;//Assignment of set and k, k++
 		}
 		return steps;
 	}
