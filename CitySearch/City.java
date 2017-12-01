08a5f32c962ca5725f556e8bffdeb3ba3c0a8367
Surya Dantuluri <surya.mvhs@gmail.com>
2017-11-30 16:35:49 -0800

    m

---
+++ CitySearch/City.java
@@ -0,0 +1,95 @@
+
+/**
+ *  City.java
+ *  Represents a City within the United States of America,
+ *  with attributes for name, state, and population.  Contains
+ *  methods for comparison (compareTo, compareToPopulation, and
+ *  equals), and a toString method.
+ *  @author Surya Dantuluri
+ *  @version 1.0
+ *  @since 11/28/2017
+ */
+public class City implements Comparable<City>
+{
+    /**    The name of the City   */
+    private String name;
+
+    /**    The state this City belongs to   */
+    private String state;
+
+    /**    The population of this City   */
+    private int population;
+    /**
+     *  Constructor for the City object
+     *
+     * @param  n  name of City
+     * @param  s  state of City
+     * @param  p  population number
+     */
+    public City(String n, String s, int p)
+    {
+        name = n;
+        state = s;
+        population = p;
+    }
+    /**
+     *  You will need to write methods to compare Cities according to name
+     *  and population.  The comparison methods will be used in the sorting
+     *  algorithms.  You may need to write other methods, as well.
+     */
+
+    /**
+     *  Compares the names of the Cities, returning a negative number if
+     *  the calling City is less than the City passed as an argument.  A
+     *  positive number is returned if the calling City is greater than
+     *  the City passed as an argument.  Use the name (primary) and state
+     *  (secondary) to make the comparison.
+     *  @param other      The City to be compared
+     *  @return           a negative integer, zero, or a positive integer
+     *                    as this City is less than, equal to, or greater
+     *                    than the City to be compared
+     */
+     public int compareTo(City other)
+     {
+        return name.compareTo(other.name);
+     }
+    /**
+     *  Compares two City objects by their populations
+     *
+     * @param  other      City object to compare to
+     * @return            positive int if population > other.population
+     *                    0 if population equals other.population
+     *                    negative int if population < other.population
+     */
+    public int compareToPopulation(City other)
+    {
+        return this.population-other.population;
+    }
+    /**
+     *  Compares the City to the specified object.  The Cities should
+     *  be considered equal if they have the same name and state
+     *  (you may disregard population).  This method should make use
+     *  of the compareTo method (see above).
+     *
+     * @param  other      City object to compare to
+     * @return            true if equal, false otherwise
+     */
+    public boolean equals(Object obj)
+    {
+        boolean isequal = false;
+        if(obj!=null)
+        {
+            if(compareTo((City)obj)==0)
+            isequal=true;
+        }
+        return isequal;
+    }
+    /**
+     *  Returns all of the information for this City as a String.
+     *  @return           The String representing the City
+     */
+    public String toString()
+    {
+        return String.format("%-22s %-4s  %7d",name,state, population);
+    }
+}
---
+++ CitySearch/CitySearch.java
@@ -0,0 +1,267 @@
+/**
+ *  CitySearch.java
+ *  E            D            I    T            H    E            R            E
+ *  @author Surya Dantuluri
+ *  @version 1.0
+ *  @since 11/28/2017
+ */
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.File;
+import java.io.IOException;
+public class CitySearch
+{
+    /**    The ArrayList of the Cities   */
+    private ArrayList<City> cities = new ArrayList<City>();
+    /**
+     *  Constructor for the CitySearch object
+     *  Loads the information from the specified file.
+     *
+     * @param  fName  name of file to opened (contains city data)
+     */
+    public CitySearch(String fName)
+    {
+        loadFile(fName);
+    }
+    /**
+     *  Constructor for the CitySearch object
+     *  Loads the information from the file "cities.txt.
+     */
+    public CitySearch ( )
+    {
+        this("cities.txt");
+    }
+    /**
+     *  Runs the program
+     */
+    public static void main(String [] args)
+    {
+        CitySearch run = new CitySearch("cities.txt");
+        run.menu();
+    }
+
+    /**
+     *  Provide the user with a menu of 4 choices (plus exit).  Prompt
+     *  the user to make a choice, and act on this choice.  Loops until
+     *  the user decides to exit.
+     */
+    public void menu ( )
+    {
+        char choice;
+        do
+        {
+            choice = getUserInput();
+            switch(choice)
+            {
+                case '1':
+                    mergeSortName(cities,0,cities.size()-1);
+                    displayCities();
+                    break;
+                case '2':
+                    insertionSortPopulation(cities);
+                    displayCities();
+                    break;
+                case '3':
+                    insertionSortPopulation(cities);
+                    displayCitiesForState();
+                    break;
+                case '4':
+                    mergeSortName(cities,0,cities.size()-1);
+                    findCity(cities);
+                    break;
+            }
+        }while(choice >= '1' && choice <= '4');
+        goodBye();
+    }
+
+    /**
+     *  Prompts the user to enter a char, from the list of '1', '2',
+     *  '3', '4', or '5'.  Keeps asking until one of these is chosen.
+     */
+    public char getUserInput ( )
+    {
+        char choice = '1';
+        System.out.println("\n\n1: Display Cities sorted by name");
+        System.out.println("2: Display Cities sorted by population");
+        System.out.println("3: Display Cities in a given state");
+        System.out.println("4: Search for one City by name");
+        System.out.println("5: Exit");
+
+        do
+        {
+            choice = Prompt.getChar("\nPlease Enter 1 through 5, indicating your choice from the menu above: ");
+        }
+        while(choice < '1' || choice > '5');
+        return choice;
+    }
+    /**
+     *  Opens the text file, and creates the city data, saving the information
+     *  into the ArrayList cities.  A typical line from cities.txt looks like this:
+     *  170,Sunnyvale,CA,147559,11.9%
+     *  This method will pull out the city name (a String), the state (a String),
+     *  and the population (an int).  These three values will then be used to
+     *  create a City object, and added to the ArrayList of cities.
+     *  @param  inFileName  name of file to opened (contains city data)
+     */
+    private void loadFile(String inFileName)
+    {
+        String temp;
+        String cityName = "";
+        String stateName = "";
+        int population = 0;
+        try
+        {
+            Scanner inFile = new Scanner(new File(inFileName));
+            inFile.nextLine();
+            while(inFile.hasNext())
+            {
+                temp = inFile.nextLine();
+                temp = temp.substring(temp.indexOf(',')+1);
+                cityName = temp.substring(0,temp.indexOf(','));
+                temp = temp.substring(temp.indexOf(',')+1);
+                stateName = temp.substring(0,temp.indexOf(','));
+                temp = temp.substring(temp.indexOf(',')+1);
+                population = Integer.parseInt(temp.substring(0,temp.indexOf(',')));
+                //System.out.println("city "+cityName);
+                //System.out.println("state "+stateName);
+                //System.out.println("pop "+population);
+                cities.add(new City(cityName,stateName,population));
+            }
+        }
+        catch(IOException e)
+        {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+    /**
+     *  Displays the cities, using a frame, and the toString method from the
+     *  City class.
+     */
+    public void displayCities ( )
+    {
+        System.out.println("\n\n\n+------------------+");
+        System.out.println("| List of Cities   |");
+        System.out.println("+----------------------------------------------+");
+        System.out.println("|                                              |");
+        System.out.println("|  Rank  City                State Population  |");
+        for (int i = 0; i < cities.size(); i++)
+        {
+            if (i%5 == 0)
+            {
+                System.out.println("|                                              |");
+            }
+            System.out.print("|  ");
+            System.out.printf("%4d  ",(i+1));
+            System.out.println(cities.get(i) + "  |");
+        }
+        System.out.println("|                                              |");
+        System.out.println("+----------------------------------------------+");
+    }
+    /**
+     *  Displays the cities from a chosen state, using a frame, and the toString
+     *  method from the City class.  The user is prompted to enter a state name
+     *  (the two-char abbreviation), then the related cities are printed.  The
+     *  user continues to be prompted, until entering -1 to exit.
+     */
+    public void displayCitiesForState ( )
+    {
+        String st = Prompt.getString("\nEnter the name of the state(-1 to exit): ");
+        while(!st.equals("-1"))
+        {
+            System.out.println("\n\n\n+------------------+");
+            System.out.println("| List of Cities   |");
+            System.out.println("+----------------------------------------------+");
+            System.out.println("|                                              |");
+            System.out.println("|  Rank  City                State Population  |");
+            int counter = 0;
+
+            //  Figure out how to print out the cities that belong to the chosen state.
+
+            System.out.println("|                                              |");
+            System.out.println("+----------------------------------------------+");
+            st = Prompt.getString("\nEnter the name of the state(-1 to exit): ");
+        }
+    }
+
+    /**
+     *  A merge sort implementation, sorting the cities by city name (primary), and
+     *  state name (secondary).
+     */
+    public void mergeSortName(ArrayList<City> list, int first, int last)
+    {
+
+    }
+
+    /**
+     *  A helper method for the mergeSortName method, merging two "halves" into
+     *  an ordered "whole".
+     */
+    private void merge(ArrayList<City> list, int first, int mid, int last)
+    {
+
+    }
+
+    /**
+     *  Swaps the elements in the ArrayList at i and j.
+     */
+    private void swap(ArrayList<City> list, int i, int j)
+    {
+        City temp = list.get(i);
+        list.set(i,list.get(j));
+        list.set(j,temp);
+    }
+
+    /**
+     *  An insertion sort implementation, sorting the cities by population.
+     */
+    public void insertionSortPopulation(ArrayList<City> list)
+    {
+    }
+
+    /**
+     *  A selection sort implementation, sorting the cities by population.
+     */
+    public void selectionSortPopulation(ArrayList<City> list)
+    {
+    }
+    /**
+     *  Prompts the user to enter the name of a city and state in the form of:
+     *  Cupertino, CA
+     *  Calls a binary search algorithm to accomplish the search.  Continues to
+     *  prompt the user until -1 is entered.
+     */
+    public void findCity(ArrayList<City> list)
+    {
+        String cityName = new String("");
+        do
+        {
+            System.out.println("\n\n----------------------------------------------------------------\n");
+            cityName = Prompt.getString("Please enter a City and State (City, State) to search for (-1 to exit): ");
+
+            //  Call the binary search algorithm (method).
+
+        }while(!cityName.equals("-1"));
+    }
+
+    /**
+     *  A binary search.  A counter is kept, so that the number of steps (times inside the loop)
+     *  can be displayed after each search.
+     */
+    public int binarySearch (ArrayList<City> list, City city)
+    {
+        int counter = 0;
+        int low = 0;
+        int high = list.size() - 1;
+        int mid;
+        //  Use a binary search algorithm to find the city.
+        System.out.println("\nThe binary search took " + counter + " steps to determine that this City does not exist in the list.");
+        return -1;
+    }
+    /**
+     *  An exit message.
+     */
+    public void goodBye ( )
+    {
+        System.out.println("\n\nThanks for reviewing the City Search results!\n\n\n");
+    }
+}
