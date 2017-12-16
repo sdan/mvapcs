984e7b5dab2ac54dd2c6cb47587f704826ffb500
Surya Dantuluri <surya.mvhs@gmail.com>
2017-12-15 23:35:08 -0800

    a

--- Human/Human.java
+++ Human/Human.java
@@ -4,15 +4,15 @@
 import java.util.Scanner;
 interface Homonid
 {
-   int efficiency(String traits, int length, int lifespan, String chromosomes, String chromatids,  boolean loraApproved, boolean retarded);
-   int diversity(int prophase, boolean doubleChromatid, int genes, String whoIsDaddy);
-   int score(String school, int population, int income, int cancerRating, int rapeRating, boolean kochleen);
-   int huntingAbility(int strength, String name, int grade, int lifespan, int raped);
+  int efficiency(String traits, int length, int lifespan, String chromosomes, String chromatids,  boolean loraApproved, boolean retarded);
+  int diversity(int prophase, boolean doubleChromatid, int genes, String whoIsDaddy);
+  int score(String school, int population, int income, int cancerRating, int rapeRating, boolean kochleen);
+  int huntingAbility(int strength, String name, int grade, int lifespan, int raped);
 
 }
 abstract class HomoSapiens implements Homonid
 {
-   boolean fail(String name, char initial)
+  boolean fail(String name, char initial)
   {
     boolean failure = false;
     if(name.toLowerCase().equals("deepak")&&initial=='b')
@@ -35,11 +35,14 @@ abstract class HomoSapiens implements Homonid
 
 public class Human extends HomoSapiens implements Homonid
 {
-  private int efficiency(String traits, int length, int lifespan, String chromosomes, String chromatids, boolean loraApproved, boolean retarded)
+  String name, school, lifespan, autism, chromosomes, chromatids, whoIsDaddy;
+  boolean loraApproved, retarded, doubleChromatid, kochleen;
+  int prophase,genes, population, income, cancerRating, rapeRating, strength, grade, lifespan, raped;
+  public int efficiency(String traits, int length, int lifespan, String chromosomes, String chromatids, boolean loraApproved, boolean retarded)
   {return 0;}
-  private int diversity(int prophase, boolean doubleChromatid, int genes, String whoIsDaddy){return 0}
-  private int score(String school, int population, int income, int cancerRating, int rapeRating, boolean kochleen){return 0}
-  private int huntingAbility(int strength, String name, int grade, int lifespan, int raped){return 0}
+  public int diversity(int prophase, boolean doubleChromatid, int genes, String whoIsDaddy){return 0;}
+  public int score(String school, int population, int income, int cancerRating, int rapeRating, boolean kochleen){return 0;}
+  public int huntingAbility(int strength, String name, int grade, int lifespan, int raped){return 0;}
 
   public static void main(String[]args)
   {
@@ -47,8 +50,117 @@ public class Human extends HomoSapiens implements Homonid
     degeneration.run();
   }
 
+  public Human()
+  {
+     name = "Deepak";
+     school = "Monta Vista High School";
+     autism = "Arnav";
+     chromosomes = "69";
+     chromatids = "Plagarism on Essay Dropped Tanabe";
+     whoIsDaddy = "Ishani";
+     race = "cancerous thing";
+     loraApproved = false;
+     retarded = true;
+     doubleChromatid = false;
+     kochleen = false;
+      raceIsReal = true;
+     prophase = 69;
+     genes = 420;
+     population = 9001
+     income = 100000
+     cancerRating = 1
+     rapeRating = 6900000
+     strength = 0
+     grade = 0
+     lifespan = 0
+     raped = 10;
+
+  }
+
+  public void printall()
+  {
+    System.out.println("----------------------------------------------------------------------");
+    System.out.println("                  PLAYER                PROFILE");
+    System.out.println("----------------------------------------------------------------------");
+    System.out.println("                  NAME: "+name);
+    System.out.println("                SCHOOL: "+school);
+    System.out.println("  AUTISTIC FRIEND NAME: "+autism);
+    System.out.println("           CHROMOSOMES: "+chromosomes);
+    System.out.println("            CHROMATIDS: "+chromatids);
+    System.out.println("          WHO IS DADDY: "+whoIsDaddy);
+    System.out.println("                  RACE: "+race);
+    System.out.println("----------------------------------------------------------------------");
+    System.out.println("  LORA LERNER APPROVED: "+loraApproved);
+    System.out.println("    RETARDED AT BIRTH?: "+retarded);
+    System.out.println("     DOUBLE CHROMATIDS: "+doubleChromatid);
+    System.out.println("  HAS OR HAD KOCHLEEN : "+kochleen);
+    if(kochleen)System.out.println("you have been degenerated");
+    System.out.println("   HAS OR HAD MUELLER : "+mueller);
+    if(mueller)System.out.println("you have been officially raped by a teacher, congrats");
+    System.out.println("BELIEVES RACE IS REAL : "+raceIsReal);
+    System.out.println("----------------------------------------------------------------------");
+    System.out.println("        PROPHASE LEVEL: "+prophase);
+    System.out.println("                 GENES: "+genes);
+    System.out.println("            POPULATION: "+population);
+    System.out.println("                INCOME: "+income);
+    System.out.println("          CANCER RAING: "+cancerRating);
+    System.out.println("           RAPE RATING: "+rapeRating);
+    System.out.println("              STRENGTH: "+strength);
+    System.out.println("                 GRADE: "+grade);
+    System.out.println("              LIFESPAN: "+lifespan);
+    System.out.println(" NUMBER OF TIMES RAPED: "+raped);
+    System.out.println("----------------------------------------------------------------------");
+  }
   public void run()
   {
-    System.out.println("Welcome!");
+    System.out.println("\n\n\nWelcome to the");
+    System.out.println("      ___           ___           ___           ___           ___");
+    System.out.println("     /__/\'         /__/\'         /__/\'         /  /\'         /__/\'");
+    System.out.println("     \'  \':\'        \'  \':\'       |  |::\'       /  /::\'        \'  \':\'   ");
+    System.out.println("      \'__\':\'        \'  \':\'      |  |:|:\'     /  /:/\':\'        \'  \':\'  ");
+    System.out.println("  ___ /  /::\'   ___  \'  \':\'   __|__|:|\':\'   /  /:/~/::\'   _____\'__\':\' ");
+    System.out.println(" /__/\'  /:/\':\' /__/\'  \'__\':\' /__/::::| \':\' /__/:/ /:/\':\' /__/::::::::\'");
+    System.out.println("  \'  \'::/       \'  \':\'  /:/   \'  \':\'        \'  \'::/       \'  \':\'  ~~~ ");
+    System.out.println("   \'  \':\'        \'  \':\'/:/     \'  \':\'        \'  \':\'        \'  \':\'     ");
+    System.out.println("    \'  \':\'        \'  \'::/       \'  \':\'        \'  \':\'        \'  \':\'    ");
+    System.out.println("     \'__\'/         \'__\'/         \'__\'/         \'__\'/         \'__\'/    ");
+    System.out.println("\ngame.\n\n Monta Vista Students as your host, and we'll be understanding your life through your choices here on the game.\n\n Keep in mind for the audience that cancer, aids, autism, rape, grades, and deepakDisease may occur while playing the game.\n\n Never play without parental supervision and always be mindfull of child predators like teachers who make sure a 89.75 is a B. \n\n\nIf rape is brought up, please be aware that this intimate experience is consensual between teacher and student when they are given the grade contract.\n\n");
+    System.out.println("\nNow let's start the game.\n");
+
+    System.out.println("What's your name?");
+    Scanner keyboard = new Scanner(System.in);
+		 name=keyboard.nextLine();
+     if(name.toLowerCase().equals("deepak"))
+     {
+       System.out.println("\nWhat's your last initial, deepak?");
+       if(fail(name,keyboard.next().charAt(0)))
+       System.out.println("\n\n\t Currently this game ONLY supports Humans.\n\n\n");
+       System.exit(2);
+     }
+     System.out.println("\n What school do you go to (type in number)?");
+     System.out.println("\n\n    Options:");
+     System.out.println("     1. Monta Vista High School");
+     System.out.println("     2. Homestead High School");
+     System.out.println("     3. Fremont High School");
+     System.out.println("     4. Lynbrook High School");
+     System.out.println("     5. Cupertino High School");
+     System.out.println("     6. Another High School");
+     switch(keyboard.nextInt())
+     {
+       case 1: school = "Monta Vista High School";
+       System.out.println("Unfortunately your lifespan decreases by 25 YEARS AUTOMATICALLY for choosing MVHS");
+       System.out.println("\nStudies have shown students who have been sleep deprived for years");
+       System.out.println("and have been raped multiple times decreases life expectancy");
+       break;
+       case 2: school = "Homestead High School";
+       break;
+       case 3: school = "Fremont High School";
+       break;
+       case 4: school = "Lynbrook High School";
+       break;
+       case 5: school = "Cupertino High School";
+       break;
+       case 6: school = "Another High School";
+     }
   }
 }
