716da4aec8c0efc6871ca9c92f4b4d698a44206b
Surya Dantuluri <surya.mvhs@gmail.com>
2018-01-07 20:28:03 -0800

    abc

--- Recursion/Sierpinski.java
+++ Recursion/Sierpinski.java
@@ -1,13 +1,13 @@
 public class Sierpinski {
 
     public static void main(String[] args) {
-        int n = Integer.parseInt(args[0]);
+        int N = Integer.parseInt(args[0]);
         double[] cx   = { 0.000, 1.000, 0.500 };
         double[] cy   = { 0.000, 0.000, 0.866 };
 
         double x = 0.0, y = 0.0;
-        for (int i = 0; i < n; i++) {
-            int r = StdRandom.uniform(3);
+        for (int i = 0; i < N; i++) {
+            int r = (int)Math.random()*4;
             x = (x + cx[r]) / 2.0;
             y = (y + cy[r]) / 2.0;
             StdDraw.point(x, y);
