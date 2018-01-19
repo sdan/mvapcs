bf93e20500dde806002a47ff88a56785a1e2c3ed
Surya Dantuluri <surya.mvhs@gmail.com>
2018-01-18 21:58:31 -0800

    update

--- TwoDArrays/X.java
+++ TwoDArrays/X.java
@@ -401,8 +401,29 @@ public class X
         while(foundplace);
     }
 
+    public boolean findSolution(int move) {
+        for (int x = 0; move <= 31 && x < board.getWidth(); x++) {
+            for (int y = 0; y < board.getHeight(); y++) {
+                for (int direction : directions) {
+                    if (board.jump(x, y, direction)) {
+                        board.copyBoard(board, solution[move]);
+                        if (! (move >= 31 && board.isOccupied(3, 3))) {
+                            if ( findSolution(move + 1)) {
+                                return true;
+                            } else {
+                                        board.jumpBack(x, y, direction);
+                                    }
+                                } else {
+                            return true;
+                                                    }
+                                            }
+                                    }
+                            }                       
+                    }
+                    
+                    return false;
+            }
 
-    
 
 
 
