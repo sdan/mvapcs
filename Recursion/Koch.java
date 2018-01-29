3459eb921aec7641867ce5dd9495e8c9a4e57521
Surya Dantuluri <surya.mvhs@gmail.com>
2018-01-28 18:09:59 -0800

    add everytthing done

--- Recursion/Koch.java
+++ Recursion/Koch.java
@@ -3,39 +3,26 @@ public static double x = 0;
 public static double y = 0;
 public static void main(String args[]) 
 	{
-		int N = Integer.parseInt(args[0]);
-		int width = 512;
-		int height = (int) (2 * width / Math.sqrt(3));
-		double size = width / Math.pow(3.0, N);
+		int n = Integer.parseInt(args[0]);
 
+		StdDraw.setCanvasSize(600,800);
+		StdDraw.picture(0.5,0.5,"mv.png",1,1);
+		sierpinski(n, 0.5, 0.5,0.5,0.0);
+	}
 
-
-
-		StdDraw.setCanvasSize(width,height);
-
-		StdDraw.line(x,y,0.0,width*Math.sqrt(3)/2);
-
-		koch(N, size,0.0);
-		koch(N, size,-120.0);
-		koch(N, size,-120.0);
-
+public static void sierpinski(int n, double length, double xc, double yc, double angle){
+		if(n==0)
+			return;
+		drawKoch(length, xc, yc, angle);
+		sierpinski(n-1, length+0.2, xc-length/5, yc-length/5, 60.0);
+		sierpinski(n-1, length-0.2, xc+length/5,yc-length/5, -120.0);
+		sierpinski(n-1, length,  xc, yc-length/5, 60.0);
 
 	}
 
 
-
-
-public static void koch(int n, double size, double angle) {
-	angle = angle * Math.PI / 180;
-	if (n == 0) 
-		StdDraw.line(x,y,x+size*Math.sin(angle),y+size*Math.cos(angle));
-	else 
-	{
-		koch(n-1, size,0.0);
-		koch(n-1, size,60.0);
-		koch(n-1, size,-120.0);
-		koch(n-1, size,60.0);
-	}
+public static void drawKoch(double size, double x1, double y1, double angle) {
+		StdDraw.picture(x1 ,y1 ,"bulbasaur.png",0.1,0.1,angle);
 }
 
 
--- Recursion/RecursiveSquares.java
+++ Recursion/RecursiveSquares.java
@@ -1,69 +1,35 @@
+/**
+ *  RecursiveArt.java
+ *  Draws Pokemon recursively using StdDraw.java
+ *  @author Surya Dantuluri
+ *  @version 1.0
+ *  @since 1/22/2018
+ */
+
 import java.awt.Color;
-public class RecursiveSquares {
 
-    // plot a square, centered on (x, y) of the given side length
-    // with a light gray background and black border
-    public static void drawSquare(double x, double y, double size, Color color) {
-        StdDraw.setPenColor(color);
-        StdDraw.filledSquare(x, y, size/2);
-        StdDraw.setPenColor(color);
-        StdDraw.square(x, y, size/2);
-    }
-
-    // plot an order n recursive squares pattern
-    // centered on (x, y) of the given side length
-    public static void draw(int n, double x, double y, double size, Color color) {
-        if (n == 0) return;
-
-        drawSquare(x, y, size, color);
-
-        // 2.2 ratio looks good
-        double ratio = 2.2;
-        Color nice = new Color(0,0,0);
-        // recursively draw 4 smaller trees of order n-1
-        int something = (int)(Math.random()*13+1);
-        System.out.println("RAND: "+something);
-        if(something==1)
-        nice = StdDraw.BLACK;
-        if(something==2)
-        nice = StdDraw.BLUE;
-        if(something==3)
-        nice = StdDraw.CYAN;
-        if(something==4)
-        nice = StdDraw.DARK_GRAY;
-        if(something==5)
-        nice = StdDraw.GRAY;
-        if(something==6)
-        nice = StdDraw.GREEN;
-        if(something==7)
-        nice = StdDraw.LIGHT_GRAY;
-        if(something==8)
-        nice = StdDraw.MAGENTA;
-        if(something==9)
-        nice = StdDraw.ORANGE;
-        if(something==10)
-        nice = StdDraw.PINK;
-        if(something==11)
-        nice = StdDraw.RED;
-        if(something==12)
-        nice = StdDraw.WHITE;
-        if(something==13)
-        nice = StdDraw.YELLOW;
-
-        draw(n-1, x - size/2, y - size/2, size/ratio, nice);    // lower left
-        draw(n-1, x - size/2, y + size/2, size/ratio, nice);    // upper left
-        draw(n-1, x + size/2, y - size/2, size/ratio, nice);    // lower right
-        draw(n-1, x + size/2, y + size/2, size/ratio, nice);    // upper right
-    }
-
-
-    // read in an integer command-line argument n and plot an order n recursive
-    // squares pattern
+public class RecursiveArt {
     public static void main(String[] args) {
-        int n = Integer.parseInt(args[0]);
-        double x = 0.5, y = 0.5;   // center of square
-        double size = 0.5;         // side length of square
-        draw(n, x, y, size, StdDraw.YELLOW);
+        int n = 4;
+        n = Integer.parseInt(args[0]);
+        StdDraw.setCanvasSize(800 ,1000);
+        StdDraw.picture(0.5,0.5,"mv.png",1,1);
+        draw(n, 0.5, 0.5, 0.5, 0);
+    }
+
+    public static void draw(int n, double x, double y, double size, double angle) {
+        if (n == 0) 
+            return;
+        drawPokemon(x, y, size, angle);
+        double ratio = 2.2;
+        draw(n-1, x - size/2, y - size/2, size/ratio, 0);    // lower left
+        draw(n-1, x - size/2, y + size/2, size/ratio, 60);    // upper left
+        draw(n-1, x + size/2, y - size/2, size/ratio, -60);    // lower right
+        draw(n-1, x + size/2, y + size/2, size/ratio, 180);    // upper right
+    }
+
+    public static void drawPokemon(double x, double y, double size, double angle) {
+    StdDraw.picture(x ,y ,"bulbasaur.png",size,size,angle);
     }
 
 }
